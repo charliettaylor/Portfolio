1f7fa26bb9bcd407eaf30cdd82285b0e87091714
Charlie Taylor <peekoecode@gmail.com>
2022-06-24 18:14:28 -0700

    move images and routes again, please

--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -2,19 +2,19 @@ export const contacts = [
 	{
 		name: 'GitHub',
 		link: 'https://github.com/Peekoe/',
-		image: './static/github.svg',
+		image: 'src/images/github.svg',
 		alt: 'GitHub Logo'
 	},
 	{
 		name: 'LinkedIn',
 		link: 'https://linkedin.com/in/charliettaylor/',
-		image: './static/linkedin.svg',
+		image: 'src/images/linkedin.svg',
 		alt: 'LinkedIn Logo'
 	},
   {
 		name: 'Email',
 		link: 'mailto:charlie@peekoe.net',
-		image: './static/email.svg',
+		image: 'src/images/email.svg',
 		alt: 'Email Symbol'
 	}
 ];
